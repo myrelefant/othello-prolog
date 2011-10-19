d381c1694f8d79960b6c1ba89a1c8cb82b1b4519
loukmane.issa@gmail.com <loukmane.issa@gmail.com@dd83d1e8-9af5-d57f-977b-61c6adb63470>
2011-10-19 01:18:00 +0000

    okokoooo

--- interface.pl
+++ interface.pl
@@ -1,40 +1,19 @@
 testJeuFinVictoireRond :- afficheJeu(rond, [], croix, [], [], []).
 testJeuFinMatchNull :- afficheJeu(rond, [32,42, 51], croix, [], [32], [33]).
 
-testJeuDebut :- ListeVide = 
-		[	
-			11, 21, 31, 41, 51, 61, 71, 81,
-		      	12, 22, 32, 42, 52, 62, 72, 82,
-		      	13, 23, 33, 43, 53, 63, 73, 83,
-		  	14, 24, 34,         64, 74, 84,
-		  	15, 25, 35,         65, 75, 85,
-		  	16, 26, 36, 46, 56, 66, 76, 86,
-		 	17, 27, 37, 47, 57, 67, 77, 87,
-		  	18, 28, 38, 48, 58, 68, 78, 88
-		],
-		
-		ListeCroix = 
+testJeuDebut :- 
+		ListeRond = 
 		[	
 			44, 55
 		],
 
-		ListeRond = 
+		ListeCroix = 
 		[	
 			45, 54
 		], 
-		afficheJeu(rond, [34, 43, 56, 65], croix, ListeVide, ListeCroix, ListeRond). 
+		afficheJeu(rond, [34, 43, 56, 65], ListeCroix, ListeRond). 
 
-testJeuMilieu :- ListeVide = 
-		[	
-			11, 21, 31, 41, 51, 61, 71, 81,
-		      	12, 22, 32, 42, 52, 62, 72, 82,
-		      	13, 23, 33,     53, 63, 73, 83,
-		  	14, 24, 34,         64, 74, 84,
-		  	15, 25, 35,         65, 75, 85,
-		  	16, 26, 36,     56, 66, 76, 86,
-		 	17, 27, 37, 47, 57, 67, 77, 87,
-		  	18, 28, 38, 48, 58, 68, 78, 88
-		],
+testJeuMilieu :- 
 		
 		ListeCroix = 
 		[	
@@ -45,8 +24,22 @@ testJeuMilieu :- ListeVide =
 		[	
 			54
 		], 
-		afficheJeu(croix, [53, 63, 64, 65], rond, ListeVide, ListeCroix, ListeRond).
+		afficheJeu(croix, [53, 63, 64, 65], ListeCroix, ListeRond).
 
+
+isCase(X) :- Jeu = 
+			[
+				11, 12, 13, 14, 15, 16, 17, 18,   
+			    21, 22, 23, 24, 25, 26, 27, 28,  
+			    31, 32, 33, 34, 35, 36, 37, 38, 
+			    41, 42, 43, 44, 45, 46, 47, 48,
+			    51, 52, 53, 54, 55, 56, 57, 58,   
+			    61, 62, 63, 64, 65, 66, 67, 68,   
+			    71, 72, 73, 74, 75, 76, 77, 78,   
+				81, 82, 83, 84, 85, 86, 87, 88
+			], member(X, Jeu).
+
+vide(X, PionsJ, PionsA):- not(member(X, PionsJ)), not(member(X, PionsA)).
 
 clearConsole :- write('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n').
 afficheTour(J) :- write('Tour: '), write(J).
@@ -56,15 +49,14 @@ afficheChoixCase :- write('\n\nQuelle case choisissez vous?').
 afficheChoixCaseError :- write('\n\n Impossible, choisissez une autre: ').
 
 
-afficheJeu(_, _, _, [], _, _) :- clearConsole, afficheVainqueur(match_null), !.
-afficheJeu(Tour, [],Vainqueur, _, _, _):- clearConsole, afficheVainqueur(Vainqueur),!.
-afficheJeu(Tour, ListeCasePossible, _, ListeVide, ListeCroix, ListeRond):- clearConsole,afficheTour(Tour), afficheCasePossible(ListeCasePossible), afficheVainqueur(inconnu), afficherGrille(11, ListeVide, ListeCroix, ListeRond), afficheChoixCase.
+afficheJeu(_, [],_, _) :- clearConsole, afficheVainqueur(match_null), !.
+afficheJeu(Tour, ListeCasePossible, ListeCroix, ListeRond):- clearConsole,afficheTour(Tour), afficheCasePossible(ListeCasePossible), afficheVainqueur(inconnu), afficherGrille(11, ListeCroix, ListeRond), afficheChoixCase.
 
-afficherGrille(X, _,_,_):- X > 88,!.
-afficherGrille(X, ListeVide, ListeCroix, ListeRond):- member(X,ListeVide), afficheCase(X, vide), findNextCase(X, Z), afficherGrille(Z, ListeVide, ListeCroix, ListeRond),!.
-afficherGrille(X, ListeVide, ListeCroix, ListeRond):- member(X,ListeCroix), afficheCase(X, croix), findNextCase(X, Z), afficherGrille(Z, ListeVide, ListeCroix, ListeRond),!.
-afficherGrille(X, ListeVide, ListeCroix, ListeRond):- member(X,ListeRond), afficheCase(X, rond), findNextCase(X, Z), afficherGrille(Z, ListeVide, ListeCroix, ListeRond),!.
-afficherGrille(X, ListeVide, ListeCroix, ListeRond).
+afficherGrille(X, _,_):- X > 88,!.
+afficherGrille(X, ListeCroix, ListeRond):- vide(X, ListeRond, ListeCroix), afficheCase(X, vide), findNextCase(X, Z), afficherGrille(Z, ListeCroix, ListeRond),!.
+afficherGrille(X, ListeCroix, ListeRond):- member(X,ListeCroix), afficheCase(X, croix), findNextCase(X, Z), afficherGrille(Z, ListeCroix, ListeRond),!.
+afficherGrille(X, ListeCroix, ListeRond):- member(X,ListeRond), afficheCase(X, rond), findNextCase(X, Z), afficherGrille(Z, ListeCroix, ListeRond),!.
+afficherGrille(X, ListeCroix, ListeRond).
 
 %Fonctions permettant dafficher une case. Plusieurs critères sont à prendre en compte:
 %Si X-1 mod 10 == 0, on est sur la première case, il faut donc passer à la ligne avant d'afficher'.
@@ -76,11 +68,13 @@ afficheCase(X,Type) :- Type == croix, retourLigne(X), write('|x|'),!.
 afficheCase(X,Type) :- Type == rond, retourLigne(X), write('|o|'),!.
 
 
-retourLigne(X) :- X < 20, write('\n'),!.
+retourLigne(X) :- eval(X-1, Res) , eval(Res mod 10, Res2), Res2=0, write('\n'), !.
 retourLigne(_).
 
 
 %Fonction permettant de trouver la prochaine case à afficher et quand il n a plus de prochaine case, on arrête la recherche.
 
-findNextCase(X, Z):- Compare = X-80, Compare<10, Compare > 0, Z is Compare+11,!.
-findNextCase(X, Z):- Z is X+10.
+findNextCase(X, Z):- eval(X-8, Compare), eval(Compare mod 10, Compare2), Compare2 \==0, Z is X+1,!.
+findNextCase(X, Z):- Z is X+3.
+
+eval(Expr,Res):-Res is Expr.
